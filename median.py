87b4ef083bdfd7ade62140bc4a8a81bb203f85f2
Anya <anya.kazi@kcl.ac.uk>
2022-12-20 16:07:19 +0400

    second try

--- median.py
+++ median.py
@@ -5,9 +5,9 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-        
+
         numbers.sort()
-        mid = len(numners) // 2
+        mid = len(numbers) // 2
         res = (numbers[mid] + numbers[~mid]) /2
         print('Median of list: ' + str(res))
 
